6cfd02aee034de2156fb9908b07dbb4f4eb290b1
louisriehl <louis.riehl@machool.com>
2023-07-27 20:58:33 -0700

    added error catching/handling

--- src/renderer/main/ManagePlayer.tsx
+++ src/renderer/main/ManagePlayer.tsx
@@ -6,6 +6,21 @@ export function ManagePlayer({players, setPlayers}): ReactElement {
   type playerMapped = { [key: string]: string; };
   let playerMap: playerMapped[] = [];
 
+  const uniq = players
+    .map((name) => {
+      return {
+        count: 1,
+        name: name
+      };
+    })
+    .reduce((result, b) => {
+      result[b.name] = (result[b.name] || 0) + b.count;
+
+      return result;
+    }, {});
+
+  const duplicates = Object.keys(uniq).filter((a) => uniq[a] > 1);
+
   playerMap = players.map((player: string, index: number) => {
     return {
       [index]: player
@@ -47,19 +62,34 @@ export function ManagePlayer({players, setPlayers}): ReactElement {
   }
 
   return (
-    <div className='tab-element'>
-      <div className='search-cancel'>
-        <input style={{width:'50%'}}value={filter} type="text" className="form-control" placeholder="Search for player" onChange={(e) => onFilterChange(e)}/>
+    <div className="tab-element">
+      {
+        duplicates.length !== 0 ?
+          (<div className="alert alert-warning d-flex align-items-center" role="alert">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                 className="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img"
+                 aria-label="Warning:">
+              <path
+                d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </svg>
+            <div>
+              Duplicate players: {duplicates.join(', ')}
+            </div>
+          </div>) : (null)
+      }
+      <div className="search-cancel">
+        <input style={{ width: '50%' }} value={filter} type="text" className="form-control"
+               placeholder="Search for player" onChange={(e) => onFilterChange(e)}/>
         <div>
-          <button className='btn btn-outline-danger' onClick={(e) => clearPlayers(e)}>Clear players</button>
+          <button className="btn btn-outline-danger" onClick={(e) => clearPlayers(e)}>Clear players</button>
         </div>
       </div>
       <hr></hr>
       <div className="list-group">
         <ul>
-          {playerList.length === 0 ? (<span style={{textAlign: 'center'}}>No players yet...</span>) : playerList}
+          {playerList.length === 0 ? (<span style={{ textAlign: 'center' }}>No players yet...</span>) : playerList}
         </ul>
       </div>
     </div>
-  )
+  );
 }
