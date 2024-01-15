7d016ae98dc815fd730c0d55af6c0c87466e31cd
Louis Riehl <42213557+louisriehl@users.noreply.github.com>
2024-01-14 20:54:12 -0800

    cleanups and git updates

--- forge.config.ts
+++ forge.config.ts
@@ -1,14 +1,12 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
+import { MakerDMG } from '@electron-forge/maker-dmg';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
-import MakerDMG from '@electron-forge/maker-dmg';
 
 const config: ForgeConfig = {
   packagerConfig: {
